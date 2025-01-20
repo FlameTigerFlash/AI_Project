01c850bd2ae8b67f2c21d4eb8bad628d1112beb8
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2025-01-20 10:03:53 +0300

    Add files via upload

--- apsched.py
+++ apsched.py
@@ -6,31 +6,29 @@ async def task_reminder(bot: Bot):
     user_rows = await db_get_items(table='users')
     for user_row in user_rows:
         participation = await db_get_items(table='user_tasks', user_id=user_row[0])
-        if len(participation)  == 0:
-            continue
-        txt = f"Добрый день! Ваши активные задания:\n"
+        txt = f"{'Добрый день! Ваши активные задания:' if len(participation) != 0 else "Добрый день! Активных задач нет."}\n"
         for el in participation:
             task = await db_get_items(table='tasks', id=el[2])
             task = task[0]
             if task[6].strip() != 'IN_PROCESS':
                 continue
-            txt += (f"ID: {el[2]};\n"
+            txt += (f"\nID: {el[2]},\n"
                     f"Название: {task[2]},\n"
                     f"Команда: {task[3]},\n"
                     f"Исполнители: {task[4]},\n"
                     f"Описание: {task[5]},\n"
                     f"Ваша роль: {el[1]}\n")
-        await bot.send_message(user_row[0], text=txt)
         notifications = await db_get_notifications(user_id=user_row[0])
         exists = False
         for el in notifications:
             if el[2] in ('Запрос', 'Ответ'):
                 exists = True
                 break
+        signed_form = await db_element_exists(table='form', user_id=user_row[0])
+        txt += '\n' * (exists + (not signed_form))
         if exists:
-            await bot.send_message(user_row[0], text="Уведомления требуют Вашего внимания! Подробнее в личном кабинете.")
-        signed_form  = await db_element_exists(table='form', user_id=user_row[0])
+            txt += 'Уведомления требуют Вашего внимания! Подробнее в личном кабинете.\n'
         if not signed_form:
-            await bot.send_message(user_row[0],
-                                   text="Если Вам не трудно, заполните, пожалуйста, анкету в личном кабинете. "
-                                        "Это поможет развитию бота в дальнейшем.")
+            txt +=("Если Вам не трудно, заполните, пожалуйста, анкету в личном кабинете. "
+                   "Это поможет развитию бота в дальнейшем.\n")
+        await bot.send_message(user_row[0], text=txt)
